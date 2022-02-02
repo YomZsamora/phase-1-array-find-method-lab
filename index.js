b53fc4771adcbdff9b23e4ebe36d447a3273c37e
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 21:31:58 +0300

    Completed Lab.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // Returns a year the KC Chiefs won the superbowl and undefined if the record has no win objects
 let superbowlWin = superbowlWinRecord => {
 	let superbowlWon = superbowlWinRecord.find( superbowl => {  return superbowl.result === "W" })
